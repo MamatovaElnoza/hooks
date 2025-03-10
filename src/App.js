4bf913460f4ca0db6a3a1c78465ef3756ce9ce28
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-03-10 10:48:50 +0500

    setState Function Count

--- src/App.js
+++ src/App.js
@@ -1,46 +1,78 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class User extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0, age: '' };
+// class User extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { counter: 0, age: '' };
+//   }
+
+//   onPlus = () => this.setState(prevState => ({ counter: prevState.counter + 1 }))
+//   onMinus = () => this.setState(prevState => ({ counter: prevState.counter - 1 }))
+//   Zero = () => this.setState({ counter: 0 })
+
+//   changeHandler = e => { this.setState({ age: e.target.value }) };
+
+//   render() {
+//     const { firstName, lastName, link } = this.props
+//     const { age, counter } = this.state
+
+//     return (
+//       <div className="w-50 mx-auto">
+//         <div className="border p-3 mt-5">
+//           <h4>Mening ismim - {firstName}, sharifim - {lastName}, yoshim - {age}</h4>
+//           <a href={link}>Youtube</a>
+//           <div className="mt-3">
+//             <button className="btn btn-success" onClick={this.onPlus}>
+//               increment
+//             </button>
+//             <button className="btn btn-danger mx-2" onClick={this.onMinus}>
+//               decrement
+//             </button>
+//             <button className="btn btn-info" onClick={this.Zero}>
+//               restart
+//             </button>
+//             <p>{counter}</p>
+//           </div>
+//           <form>
+//             <span>Yoshingizni kursating:</span>
+//             <input className="form-control" type="text" onChange={e => this.changeHandler(e, 'sammi')} />
+//           </form>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+const User = ({ firstName, lastName, link }) => {
+  const [counter, setCount] = useState(0)
+  const [age, setAge] = useState(21)
+
+  const onIncrement = () => {
+    setCount(counter + 1)    
   }
 
-  onPlus = () => this.setState(prevState => ({ counter: prevState.counter + 1 }))
-  onMinus = () => this.setState(prevState => ({ counter: prevState.counter - 1 }))
-  Zero = () => this.setState({ counter: 0 })
+  const onDicrement = () => {
+    setCount(counter - 1)
+  }
 
-  changeHandler = e => { this.setState({ age: e.target.value }) };
-
-  render() {
-    const { firstName, lastName, link } = this.props
-    const { age, counter } = this.state
-
-    return (
-      <div className="w-50 mx-auto">
-        <div className="border p-3 mt-5">
-          <h4>Mening ismim - {firstName}, sharifim - {lastName}, yoshim - {age}</h4>
-          <a href={link}>Youtube</a>
-          <div className="mt-3">
-            <button className="btn btn-success" onClick={this.onPlus}>
-              increment
-            </button>
-            <button className="btn btn-danger mx-2" onClick={this.onMinus}>
-              decrement
-            </button>
-            <button className="btn btn-info" onClick={this.Zero}>
-              restart
-            </button>
-            <p>{counter}</p>
-          </div>
-          <form>
-            <span>Yoshingizni kursating:</span>
-            <input className="form-control" type="text" onChange={e => this.changeHandler(e, 'sammi')} />
-          </form>
+  const onRestart = () => {
+    setCount(0)
+  }
+  
+  return (
+    <div className="w-50 mx-auto">
+      <div className="border p-3 mt-5">
+        <h1>Mening ismim - {firstName}, sharifim - {lastName}, yoshim - {age}</h1>
+        <a href={link}>Youtube</a>
+        <p className="text-center">{counter}</p>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-success" onClick={onIncrement}>+</button>
+          <button className="btn btn-danger mx-2" onClick={onDicrement}>-</button>
+          <button className="btn btn-warning" onClick={onRestart}>+</button>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 
