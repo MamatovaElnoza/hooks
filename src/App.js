dc23fbd2b010452376d11ed05931b3aedbf7b7ac
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-03-13 10:42:43 +0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Component, useEffect } from "react";
+import React, { useState } from "react";
 
 // class User extends Component {
 //   constructor(props) {
@@ -58,45 +58,16 @@ import React, { useState, Component, useEffect } from "react";
 
 const User = ({ firstName, lastName, link }) => {
   const [counter, setCount] = useState(0)
-  const [login, setLogin] = useState(true)
 
-  const onLogin = () => {
-    setLogin(prevState => !prevState)
-  }
-
-  const onIncrement = () => {
-    setCount(prevCount => prevCount + 1)    
-  }
-
-  const onDicrement = () => {
-    setCount(prevCount => prevCount - 1)    
-  }
-
-  const onRestart = () => {
-    setCount(0)
-  }
-
-  useEffect(() => {
-    console.log('effect');
-    document.title = `Counter: ${counter}`
-
-    return() => {console.log('deleted');
-    }
-  }, [])
+  const onIncrement = () => setCount(prevState => prevState + 1)
 
   return (
     <div className="w-50 mx-auto">
       <div className="border p-3 mt-5">
-        <h1>Mening ismim - {firstName}, sharifim - {lastName}</h1>
-        <a href={link}>Youtube</a>
         <p className="text-center">{counter}</p>
         <div className="d-flex justify-content-center">
           <button className="btn btn-success" onClick={onIncrement}>+</button>
-          <button className="btn btn-danger mx-2" onClick={onDicrement}>-</button>
-          <button className="btn btn-warning" onClick={onRestart}>+</button>
         </div>
-        {login ? <p>Login</p> : null}
-        <button onClick={onLogin} className="btn btn-outline-dark">login</button>
       </div>
     </div>
   )
@@ -104,16 +75,10 @@ const User = ({ firstName, lastName, link }) => {
 
 
 const App = () => {
-  const [isDisplay, setIsDisplay] = useState(true)
 
-  const deleteDisplayHandler = () => {
-    setIsDisplay(false)
-  }
-  
   return (
     <>
-    <button onClick={deleteDisplayHandler}>display</button>
-      {isDisplay && <User firstName="elnoza" lastName="mamatova" link="youtube.com" />}
+      <User firstName="elnoza" lastName="mamatova" link="youtube.com" />
     </>
   )
 }
