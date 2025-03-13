50752504097620d8942fc9ffa25ce95e8fd1cdd8
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-03-13 10:45:42 +0500

    toggle button

--- src/App.js
+++ src/App.js
@@ -56,17 +56,25 @@ import React, { useState } from "react";
 //   }
 // }
 
-const User = ({ firstName, lastName, link }) => {
+const User = () => {
   const [counter, setCount] = useState(0)
+  const [active, setActive] = useState(true)
 
   const onIncrement = () => setCount(prevState => prevState + 1)
+  const onToggle = () => setActive(prevState => !prevState)
+
+  const colors = {
+    fontWeight: 'bold',
+    color: active ? 'green' : 'red'
+  }
 
   return (
     <div className="w-50 mx-auto">
       <div className="border p-3 mt-5">
-        <p className="text-center">{counter}</p>
+        <p className="text-center" style={colors}>User activated: {counter}</p>
         <div className="d-flex justify-content-center">
-          <button className="btn btn-success" onClick={onIncrement}>+</button>
+          <button className="btn btn-success" onClick={onIncrement}>Increase</button>
+          <button className="btn btn-warning" onClick={onToggle}>Toggle</button>
         </div>
       </div>
     </div>
