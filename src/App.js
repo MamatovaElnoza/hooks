7217b0ec2262092ed7babb75f10c6e4f06976a61
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-03-19 13:25:09 +0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,67 +1,4 @@
-import React, { useState, useMemo } from "react";
-
-// class User extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { counter: 0, age: '' };
-//   }
-
-//   onPlus = () => this.setState(prevState => ({ counter: prevState.counter + 1 }))
-//   onMinus = () => this.setState(prevState => ({ counter: prevState.counter - 1 }))
-//   Zero = () => this.setState({ counter: 0 })
-
-//   changeHandler = e => { this.setState({ age: e.target.value }) };
-
-//   componentDidMount() {
-//     document.title = `Counter: ${this.state.counter}`
-//     console.log('mounting');
-//   }
-//   componentDidUpdate() {
-//     document.title = `Counter: ${this.state.age}`
-//     console.log('update');
-//   }
-//   componentWillUnmount(){
-//     alert('component will unmount!');
-//     console.log('unmount');
-//   }
-
-//   render() {
-//     const { firstName, lastName, link } = this.props
-//     const { age, counter } = this.state
-
-//     return (
-//       <div className="w-50 mx-auto">
-//         <div className="border p-3 mt-5">
-//           <h4>Mening ismim - {firstName}, sharifim - {lastName}, yoshim - {age}</h4>
-//           <a href={link}>Youtube</a>
-//           <div className="mt-3">
-//             <button className="btn btn-success" onClick={this.onPlus}>
-//               increment
-//             </button>
-//             <button className="btn btn-danger mx-2" onClick={this.onMinus}>
-//               decrement
-//             </button>
-//             <button className="btn btn-info" onClick={this.Zero}>
-//               restart
-//             </button>
-//             <p>{counter}</p>
-//           </div>
-//           <form>
-//             <span>Yoshingizni kursating:</span>
-//             <input className="form-control" type="text" onChange={e => this.changeHandler(e, 'sammi')} />
-//           </form>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-const bigCountNumber = number => {
-  console.log('render');
-  let i = 0
-  while (i < 100000000) i++
-  return number * 2
-}
+import React, { useState } from "react";
 
 const User = () => {
   const [counter, setCount] = useState(0)
@@ -75,13 +12,10 @@ const User = () => {
     color: active ? 'green' : 'red'
   }
 
-  const number = useMemo(() => bigCountNumber(counter), [counter])
-  // const number = bigCountNumber(counter)
-
   return (
     <div className="w-50 mx-auto">
       <div className="border p-3 mt-5">
-        <p className="text-center" style={colors}>User activated: {number}</p>
+        <p className="text-center" style={colors}>User activated: {counter}</p>
         <div className="d-flex justify-content-center">
           <button className="btn btn-success" onClick={onIncrement}>Increase</button>
           <button className="btn btn-warning" onClick={onToggle}>Toggle</button>
