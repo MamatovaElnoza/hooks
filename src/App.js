448fe02467c2c436e90269b5a9491d3453156015
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-03-19 13:35:26 +0500

    useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 const User = () => {
-  const [counter, setCount] = useState(0)
-  const [active, setActive] = useState(true)
+  const cvcRef = useRef(null)
 
-  const onIncrement = () => setCount(prevState => prevState + 1)
-  const onToggle = () => setActive(prevState => !prevState)
-
-  const colors = {
-    fontWeight: 'bold',
-    color: active ? 'green' : 'red'
-  }
-
+  useEffect(() => {
+    console.log(cvcRef.current);
+    
+  })
+  
   return (
     <div className="w-50 mx-auto">
       <div className="border p-3 mt-5">
-        <p className="text-center" style={colors}>User activated: {counter}</p>
-        <div className="d-flex justify-content-center">
-          <button className="btn btn-success" onClick={onIncrement}>Increase</button>
-          <button className="btn btn-warning" onClick={onToggle}>Toggle</button>
-        </div>
+        <input type="text" className="form-control" placeholder="Card number"/>
+        <input ref={cvcRef} type="text" className="form-control mt-2" placeholder="Secure number"/>
       </div>
     </div>
   )
