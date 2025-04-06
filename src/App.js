9dab0ff3c816f51591fd2ea88f6e0539513c9b9d
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-04-07 00:46:53 +0500

    Validation for Number in Name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import './App.css'
 
-const User = () => {
-  const [cardNumber, setCardNumber] = useState('')
+const App = () => {
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
 
-  const cvcRef = useRef(null)
-
-  const HandleInput = e =>{
-    const val = e.target.value
-    setCardNumber(val)
-    if(val.length === 16){
-      cvcRef.current.focus()
+  const validateInput = text => {
+    if (text.search(/\d/) > - 0) {
+      return true
+    } else {
+      return false
     }
-  } 
+  }
+
+  const validateColorFirstname = validateInput(firstname) ? 'text-danger' : null
+  const validateColorLastname = validateInput(lastname) ? 'text-danger' : null
+
   return (
     <div className="w-50 mx-auto">
       <div className="border p-3 mt-5">
         <h1 className="center mb-3 fs-3">Register</h1>
         <div className="d-flex gap-3">
-        <input type="text" className="form-control" placeholder="Firstname" onChange={HandleInput} value={cardNumber}/>
-        <input ref={cvcRef} type="text" className="form-control" placeholder="Lastname"/>
+          <input type="text" className={`form-control ${validateColorFirstname}`} placeholder="Firstname" onChange={e => setFirstname(e.target.value)} value={firstname} />
+          <input type="text" className={`form-control ${validateColorLastname}`} placeholder="Lastname" value={lastname} onChange={e => setLastname(e.target.value)}/>
         </div>
         <button className="btn btn-success mt-3">Send Data</button>
       </div>
@@ -27,9 +30,5 @@ const User = () => {
   )
 }
 
-
-const App = () => {
-  return <User firstName="elnoza" lastName="mamatova" link="youtube.com" />
-}
 
 export default App;
