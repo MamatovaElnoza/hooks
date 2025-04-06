9aef3495b30eeea80023eb92eb3646bbbed53cb2
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-04-07 00:28:59 +0500

    again

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ const User = () => {
   return (
     <div className="w-50 mx-auto">
       <div className="border p-3 mt-5">
-        <h1 className="center my-2">Register</h1>
+        <h1 className="center mb-3 fs-3">Register</h1>
+        <div className="d-flex gap-3">
         <input type="text" className="form-control" placeholder="Firstname" onChange={HandleInput} value={cardNumber}/>
-        <input ref={cvcRef} type="text" className="form-control mt-2" placeholder="Lastname"/>
-        <button className="btn btn-success my-3">Send Data</button>
+        <input ref={cvcRef} type="text" className="form-control" placeholder="Lastname"/>
+        </div>
+        <button className="btn btn-success mt-3">Send Data</button>
       </div>
     </div>
   )
